722b240fb5ffbbd2fbd2a3758655733694304f43
João Iacillo <joaopiacillo@outlook.com.br>
2023-10-27 18:08:17 +0000

    Create failing filterByTerm test

---
+++ tests/filterByTerm.test.js
@@ -0,0 +1,13 @@
+describe("Filter function", () => {
+    it("should filter by a search term (url)", () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+        ];
+
+        const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+        expect(filterByTerm(input, "link")).toEqual(output);
+    });
+});
